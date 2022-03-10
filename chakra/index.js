c13bcd7d79f7e28b0598a3c20952b1c254107d59
Kevin <kevinpuchaicela@gmail.com>
2022-03-10 18:52:29 +0100

    feat: setup chakra configuration

---
+++ chakra/index.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const overrides = {
+  // Aquí se importará o se pondrá directamente el "customize" de chakra (colors, breakpoints, fonts, etc)
+  components: {
+    // componentes de chakra aquí
+  }
+}
+
+export default extendTheme(overrides)
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
+import theme from '../chakra/index'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider {...theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
