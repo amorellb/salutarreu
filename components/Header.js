5a3f79e4dd64cb4d2689a997359af34959c1d67c
amorellb <amorellb@cifpfbmoll.eu>
2022-03-11 21:07:45 +0100

    Add header basic structure based on chakra components and also add header styles

--- components/Header.js
+++ components/Header.js
@@ -4,33 +4,37 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <header>
-      <Flex>
-        <Link href="/">
-          <Image
-            src="/images/logo/logotip/logotip_fonstransparent.png"
-            alt="Corporative logo image"
-            width={200}
-            height={75}
-          />
-        </Link>
-        <Spacer />
-        <Stack direction="row" spacing={6} align="center">
-          <Link href="/">Inicio</Link>
-          <Link href="/sessions">Sesiones</Link>
-          <Link href="/aboutUs">Nosotros</Link>
-        </Stack>
-        <Spacer />
-        <Stack direction="row" spacing={6} align="center">
-          <Button colorScheme="teal" variant="outline">
-            Iniciar sesión
-          </Button>
-          <Button colorScheme="teal" variant="solid">
-            Registrarse
-          </Button>
-        </Stack>
-      </Flex>
-    </header>
+    <Flex as="header" p={5}>
+      <Link href="/">
+        <Image
+          src="/images/logo/logotip/logotip_fonstransparent.png"
+          alt="Corporative logo image"
+          width={200}
+          height={75}
+        />
+      </Link>
+      <Spacer />
+      <Stack
+        as="nav"
+        direction="row"
+        spacing={6}
+        align="center"
+        fontWeight="bold"
+      >
+        <Link href="/">Inicio</Link>
+        <Link href="/sessions">Sesiones</Link>
+        <Link href="/aboutUs">Nosotros</Link>
+      </Stack>
+      <Spacer />
+      <Stack direction="row" spacing={6} align="center">
+        <Button colorScheme="teal" variant="outline">
+          Iniciar sesión
+        </Button>
+        <Button colorScheme="teal" variant="solid">
+          Registrarse
+        </Button>
+      </Stack>
+    </Flex>
   )
 }
 
