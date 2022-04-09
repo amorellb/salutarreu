eee5bd302b8cb2f1718b6cb0dfd701fb75f8f38b
Victoria <mpelaez@cifpfbmoll.eu>
2022-04-09 13:03:50 +0200

    carrusel style and responsive

--- components/landing/Carrusel.js
+++ components/landing/Carrusel.js
@@ -28,8 +28,8 @@ export default function CaptionCarousel() {
   const [slider, setSlider] = React.useState('');
   // These are the breakpoints which changes the position of the
   // buttons as the screen size changes
-  const top = useBreakpointValue({ base: '90%', md: '50%' });
-  const side = useBreakpointValue({ base: '30%', md: '40px' });
+  const top = useBreakpointValue({ base: '85%', md: '50%', xl:'65%' });
+  const side = useBreakpointValue({ base: '20%', md: '40px', xl:'20%' });
 
   const cards = [
     {
@@ -47,9 +47,9 @@ export default function CaptionCarousel() {
         'https://github.com/amorellb/salutarreu/blob/37-landing-page-components/public/images/landing/11.png?raw=true',
     },
     {
-      title: 'Cardio',
+      title: 'Entrenamiento cardiovascular',
       text:
-        "Los ejercicios de entrenamiento cardio, son los que aumentan nuestro ritmo cardíaco, incrementan nuestra capacidad de resistencia, y mejoran nuestra condición física..",
+        "Los ejercicios de entrenamiento cardio, son los que aumentan nuestro ritmo cardíaco, incrementan nuestra capacidad de resistencia, y mejoran nuestra condición física. Ayuda a controlar la presión arterial y fortalece el sistema inmunológico. Baja la presión arterial y la frecuencia cardiaca en reposo. ",
       image:
         'https://github.com/amorellb/salutarreu/blob/37-landing-page-components/public/images/landing/12.png?raw=true',
     },
@@ -58,11 +58,11 @@ export default function CaptionCarousel() {
   return (
     <Box
       position={'relative'}
-      height={'600px'}
-      width={'80%'}
+      height={{ base: '700px', md: '900px' }}
+      width={'100%'}
       overflow={'hidden'}
-      margin={'200px'}>
-      
+      margin='100px'>
+
       {/* CSS files for react-slick */}
       <link
         rel="stylesheet"
@@ -110,29 +110,55 @@ export default function CaptionCarousel() {
             key={index}
             height={'xl'}
             position="relative"
-            backgroundPosition="center"
+            backgroundPosition={{ base: "right", md: "left" }}
             backgroundRepeat="no-repeat"
-            backgroundSize="cover"
+            backgroundSize={{ sm: "cover", md: "cover", lg: "contain" }}
             backgroundImage={`url(${card.image})`}
-            >
+
+          >
             {/* This is the block you need to change, to customize the caption */}
-            <Container size="container.lg" height="600px" position="relative">
+            <Container size="container.xl" height="600px" position="relative">
               <Stack
-                spacing={3}
-                w={'700px'}
-                maxW={'100%'}
+                spacing={5}
+                w={'800px'}
+                maxWidth={'100%'}
                 position="absolute"
-                top="70%"
-                transform="translate(0, -50%)">
-                <Heading 
-                fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}
-                color={'black'}
+                top={{ base: "70%", md: '50%', lg: '60%' }}
+                transform={{ base: "translate(0, -60%)", md: "translate(5%, 20%)", lg: "translate(-10%, -10%)", xl:"translate(55%, -80%)" }}
+              >
+                <Heading
+                  as={'h2'}
+                  lineHeight={1.1}
+                  fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
                 >
-                  {card.title}
+                  <Text
+                    as={'span'}
+                    position={'relative'}
+                    _after={{
+                      content: "''",
+                      width: 'full',
+                      height: '30%',
+                      position: 'absolute',
+                      bottom: 1,
+                      left: 0,
+                      bg: 'brand.400',
+                      zIndex: -1
+                    }}
+                  >
+                    {card.title}
+                  </Text>
                 </Heading>
-                <Text 
-                fontSize={{ base: 'md', lg: 'lg' }} 
-                color="brand.600">
+                <Text
+                  width={{ base: '400px', md: '480px', lg: '650px', xl:'550px' }}
+                  fontSize={{ base: 'md', lg: 'lg' }}
+                  color="brand.600"
+                  fontWeight={'bold'}
+                  p={'10px'}
+                  borderRadius={'10px'}
+                  backgroundColor={'whiteAlpha.300'}
+                  textAlign={'justify'}
+                  paddingLeft={5}
+                >
                   {card.text}
                 </Text>
               </Stack>
