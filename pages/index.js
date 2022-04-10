87e918f44fd3c827d4a020aeee87bd8c94a8223b
Magi <mpayeras@cifpfbmoll.eu>
2022-04-11 00:46:15 +0200

    Footer

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import { Container, Heading, Stack, Text } from '@chakra-ui/react'
 
 export default function Home() {
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Salut Arreu</title>
@@ -32,19 +33,9 @@ export default function Home() {
         <Carrusel />
         <Testimonials />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+   
     </div>
+
+
   )
 }
