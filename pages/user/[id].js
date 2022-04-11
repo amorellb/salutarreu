b67fd4c2b227a8f49419a84342d96e826d3c92f2
amorellb <amorellb@cifpfbmoll.eu>
2022-04-11 08:46:11 +0200

    user's page section

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,3 +1,13 @@
-function UserPage() {}
+import { Box } from '@chakra-ui/react'
+
+function UserPage() {
+  return (
+    <Box as="main">
+      <Box as="section">Tabla con los usuarios (solo entrenado)</Box>
+
+      <Box as="section">Calendario de sesiones</Box>
+    </Box>
+  )
+}
 
 export default UserPage
