55b236fc165b469e69dc73074d3548a21d7f1c0c
amorellb <amorellb@cifpfbmoll.eu>
2022-04-14 18:55:26 +0200

    feat: Add sidebar on user page

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,11 +1,36 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
+import Sidebar from '../../components/user/Sidebar'
 
 function UserPage() {
   return (
     <Box as="main">
-      <Box as="section">Tabla con los usuarios (solo entrenado)</Box>
+      <Sidebar>
+        <Box as="section">
+          <Heading
+            as={'h2'}
+            lineHeight={1.1}
+            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
+            bgGradient="linear(to-r,brand.600, brand.500, brand.400, brand.300)"
+            bgClip="text"
+          >
+            Listado de usuarios
+          </Heading>
+          Tabla con los usuarios (solo entrenado)
+        </Box>
 
-      <Box as="section">Calendario de sesiones</Box>
+        <Box as="section">
+          <Heading
+            as={'h2'}
+            lineHeight={1.1}
+            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
+            bgGradient="linear(to-r,brand.600, brand.500, brand.400, brand.300)"
+            bgClip="text"
+          >
+            Entrenamientos
+          </Heading>
+          Calendario de sesiones
+        </Box>
+      </Sidebar>
     </Box>
   )
 }
