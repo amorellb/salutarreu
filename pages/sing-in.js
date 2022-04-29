dd5453de63f07607ff4fa7ed9f2b4f6aed90c7b7
Magi <mpayeras@cifpfbmoll.eu>
2022-04-29 18:07:50 +0200

    resolve bugs 2

--- pages/sing-in.js
+++ pages/sing-in.js
@@ -1,24 +1,24 @@
-// import { getProviders, signIn } from 'next-auth/react'
-// import Container from '../components/styles/Container'
+import { getProviders } from 'next-auth/react'
+import { Container } from '@chakra-ui/react'
 
-// export default function SignIn({ providers }) {
-//   return (
-//     <Container>
-//       <SignInStyles>
-//         {Object.values(providers).map(provider => (
-//           <button onClick={() => signIn(provider.id)} key={provider.name}>
-//             Sign in with {provider.name}
-//           </button>
-//         ))}
-//       </SignInStyles>
-//     </Container>
-//   )
-// }
+export default function SignIn({ providers }) {
+  return (
+    <Container>
+      {/* <SignInStyles>
+        {Object.values(providers).map(provider => (
+          <button onClick={() => signIn(provider.id)} key={provider.name}>
+            Sign in with {provider.name}
+          </button>
+        ))}
+      </SignInStyles> */}
+    </Container>
+  )
+}
 
-// // This is the recommended way for Next.js 9.3 or newer
-// export async function getServerSideProps(context) {
-//   const providers = await getProviders()
-//   return {
-//     props: { providers }
-//   }
-// }
+// This is the recommended way for Next.js 9.3 or newer
+export async function getServerSideProps(context) {
+  const providers = await getProviders()
+  return {
+    props: { providers }
+  }
+}
