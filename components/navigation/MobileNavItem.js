84406f10281d82b93621e256fe994fb6fe9bbfcd
kevinrp22 <kevinpuchaicela@gmail.com>
2022-05-03 23:37:27 +0200

    nextjs Link component was not used

--- components/navigation/MobileNavItem.js
+++ components/navigation/MobileNavItem.js
@@ -1,20 +1,21 @@
-import { Flex, Link, Stack, Text, useDisclosure } from '@chakra-ui/react'
+import { Flex, Stack, Text, useDisclosure } from '@chakra-ui/react'
+import Link from 'next/link'
 
 function MobileNavItem({ label, href }) {
   const { onToggle } = useDisclosure()
 
   return (
     <Stack spacing={4} onClick={onToggle}>
-      <Flex
-        as={Link}
-        py={2}
-        href={href ?? '#'}
-        justify={'space-between'}
-        align={'center'}
-        _hover={{ textDecoration: 'none' }}
-      >
-        <Text fontWeight={600}>{label}</Text>
-      </Flex>
+      <Link href={href} passHref>
+        <Flex
+          py={2}
+          justify={'space-between'}
+          align={'center'}
+          _hover={{ textDecoration: 'none' }}
+        >
+          <Text fontWeight={600}>{label}</Text>
+        </Flex>
+      </Link>
     </Stack>
   )
 }
