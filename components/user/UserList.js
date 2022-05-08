e4f242dfbf561544153f5dffdc4ca1d0695b5ea0
Victoria <mpelaez@cifpfbmoll.eu>
2022-05-08 13:34:32 +0200

    table userList mediaQuery, styles

--- components/user/UserList.js
+++ components/user/UserList.js
@@ -7,84 +7,140 @@ import {
   Box,
   Icon,
   Button,
-  Heading
+  Heading,
+  useMediaQuery
 } from "@chakra-ui/react";
 import { FiTrash2, FiUser } from "react-icons/fi";
 
 export default function UserList({ users }) {
-  console.log(users);
   const [page, setPage] = React.useState(1);
+  const [isLessThan900px] = useMediaQuery('(min-width: 900px)')
 
   const tableData = users.map((user) => ({
     name: (
       <Flex align="center">
-        <Avatar name={user.name} src={user.avatar} size="sm" mr="4" />
-        <Text>{user.name}</Text>
+        <Avatar name={user.name} src={user.avatar} size="md" mr="4" />
+        <Text fontSize={'small'}>{(user.name).toUpperCase()}</Text>
       </Flex>
     ),
-    email: user.email,
-    perfilButton: (
-      <Button
-        colorScheme="gray"
-        onClick={() => console.log("perfil user!")}
-        size="sm"
-      >
-        <Icon as={FiUser} fontSize="20" />
-      </Button>
+    email: (
+      <Flex>
+        <Text>{user.email}</Text>
+      </Flex>
     ),
-    deleteButton: (
-      <Button
-        
-        backgroundColor={'red'}
-        onClick={() => console.log("delete user!")}
-        size="sm"
-      >
-        <Icon as={FiTrash2} fontSize="20" />
-      </Button>
-    )
+    perfilButton: (
+      <Flex>
+        <Button
+          colorScheme="gray"
+          onClick={() => console.log("perfil user!")}
+          size="sm"
+          marginRight={2}
+        >
+          <Icon as={FiUser} fontSize="20" />
+        </Button>
+        <Button
+          backgroundColor={'red'}
+          onClick={() => console.log("delete user!")}
+          size="sm"
+          _hover={{ bgColor: 'red.800' }}
+        >
+          <Icon as={FiTrash2} fontSize="20" />
+        </Button>
+      </Flex>
+    ),
+  }));
+
+  const tableDataSmall = users.map((user) => ({
+    name: (
+      <Box align="center">
+        <Avatar name={user.name} src={user.avatar} size="md" mb={2} />
+        <Text fontSize={'small'}>{(user.name).toUpperCase()}</Text>
+        <Text fontSize={'small'}>{user.email}</Text>
+      </Box>
+    ),
+    perfilButton: (
+      <Flex>
+        <Button
+          colorScheme="gray"
+          onClick={() => console.log("perfil user!")}
+          size="sm"
+          marginRight={2}
+        >
+          <Icon as={FiUser} fontSize="20" />
+        </Button>
+        <Button
+          backgroundColor={'red'}
+          onClick={() => console.log("delete user!")}
+          size="sm"
+          _hover={{ bgColor: 'red.800' }}
+        >
+          <Icon as={FiTrash2} fontSize="20" />
+        </Button>
+      </Flex>
+    ),
   }));
 
   const tableColumns = [
-    
     {
-      Header: "Nombre",
+      Header: "",
       accessor: "name"
     },
     {
-      Header: "Email",
+      Header: "",
       accessor: "email"
     },
     {
       Header: "",
-      accessor: "perfilButton"
-    }
-    ,
-    {
-      Header: "",
-      accessor: "deleteButton"
-    }
+      accessor: "perfilButton",
+    },
+
   ];
 
   return (
-    <Box p="12">
-      <Heading size="md" as="h2">
+    <Box py="10" overflow={'auto'}>
+      <Heading
+        fontSize={'2xl'}
+        as="h2"
+        bgGradient="linear(to-r, black, brand.400,brand.600, brand.300, brand.500,brand.300,brand.400, brand.600, brand.500, brand.400, brand.300,brand.600, black)"
+        bgClip="text" fontWeight="900"
+        lineHeight={1.2}
+        paddingStart={'50px'}
+        fontStyle={'italic'}
+      >
         Lista de usuarios
       </Heading>
 
-      <Box mt="6" >
-        <Table
-          colorScheme="blue"
-          emptyData={{
-            icon: FiUser,
-            text: "Nobody is registered here."
-          }}
-          totalRegisters={users.length}
-          page={page}
-          onPageChange={(page) => setPage(page)}
-          columns={tableColumns}
-          data={tableData}
-        />
-      </Box>
+      {isLessThan900px ? (
+        <Box mt="6">
+          <Table
+            colorScheme="blue"
+            emptyData={{
+              icon: FiUser,
+              text: "Nobody is registered here."
+            }}
+            totalRegisters={users.length}
+            page={page}
+            onPageChange={(page) => setPage(page)}
+            columns={tableColumns}
+            data={tableData}
+          />
+        </Box>
+      ) : (
+        <Box mt="6">
+          <Table
+            colorScheme="blue"
+            emptyData={{
+              icon: FiUser,
+              text: "Nobody is registered here."
+            }}
+            totalRegisters={users.length}
+            page={page}
+            onPageChange={(page) => setPage(page)}
+            columns={tableColumns}
+            data={tableDataSmall}
+          />
+        </Box>
+      )}
     </Box>
   );
 }
