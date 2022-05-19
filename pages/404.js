fa35f053a801f5ef54667c2dbd26e42bc7b655c2
Victoria <mpelaez@cifpfbmoll.eu>
2022-05-19 20:39:43 +0200

    fix: error pages style padding and height

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,15 @@
-import { Container, Heading, useBreakpointValue } from '@chakra-ui/react'
+import { Container, Heading } from '@chakra-ui/react'
 
 export default function error404() {
   return (
-    <Container as="main" py={{ base: '16.5rem', md: '22rem' }}>
+    <Container as="main" h={'65.5vh'} display='flex' alignItems={'center'} justifyContent={'center'}>
       <Heading
         as="h1"
-        bgGradient="linear(to-l, black, brand.400,brand.600, brand.300, brand.500,brand.300,brand.400, brand.600, brand.500, brand.400, brand.300,brand.600, black)"
+        bgGradient="linear(to-r,brand.600, brand.500, brand.400, brand.300)"
         bgClip="text"
         fontWeight="900"
         lineHeight={1.2}
-        textAlign="center"
-        fontSize={useBreakpointValue({ base: '5xl', sm: '3xl', md: '3xl' })}
-        fontStyle={'italic'}
-        mb="3"
+        fontSize={{ base: '2xl', md: '3xl' }}
       >
         404 - PÁGINA NO ENCONTRADA
       </Heading>
