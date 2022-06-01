e28150ef7a5c0e51410c3f878b6f442cf7d1d899
Victoria <mpelaez@cifpfbmoll.eu>
2022-06-01 22:12:27 +0200

    add scroll to top

---
+++ components/ScrollToTop.js
@@ -0,0 +1,33 @@
+
+import { useEffect, useState } from "react";
+import { FaAngleUp } from "react-icons/fa";
+
+export default function ScrollToTop() {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <div className="top-to-btm">
+      {" "}
+      {showTopBtn && (
+        <FaAngleUp
+          className="icon-position icon-style"
+          onClick={goToTop}
+        />
+      )}{" "}
+    </div>
+  );
+}
